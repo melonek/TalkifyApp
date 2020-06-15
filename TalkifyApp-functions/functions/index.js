68674e4adf10919df63fe088d4ed224fdeaf7daf
melon <meloniusz@hotmail.com>
2020-06-15 17:53:52 +0800

    successfully changed get and post using express routes in api

--- TalkifyApp-functions/functions/index.js
+++ TalkifyApp-functions/functions/index.js
@@ -2,14 +2,11 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello world");
-});
 
-exports.getScreams = functions.https.onRequest((req, res) => {
+const express = require("express");
+const app = express();
+
+app.get("/screams", (req, res) => {
   admin
     .firestore()
     .collection("screams")
@@ -23,3 +20,25 @@ exports.getScreams = functions.https.onRequest((req, res) => {
     })
     .catch((err) => console.error(err));
 });
+
+app.post("/scream", (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+
+  admin
+    .firestore()
+    .collection("screams")
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
+
+exports.api = functions.https.onRequest(app);
