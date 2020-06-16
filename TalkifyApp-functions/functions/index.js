18b607a10a38bb1a4be9687d96084dc88bf650a8
melon <meloniusz@hotmail.com>
2020-06-16 20:49:58 +0800

    able to retrive token after login

--- TalkifyApp-functions/functions/index.js
+++ TalkifyApp-functions/functions/index.js
@@ -151,13 +151,18 @@ app.post("/login", (req, res) => {
     .auth()
     .signInWithEmailAndPassword(user.email, user.password)
     .then((data) => {
-      return data.getIdToken();
+      return data.user.getIdToken();
     })
     .then((token) => {
       return res.json({ token });
     })
     .catch((err) => {
       console.error(err);
+      if (err.code === "auth/wrong-password") {
+        return res
+          .status(430)
+          .json({ general: "Wrong credentials, please try again" });
+      }
       return res.status(500).json({ error: err.code });
     });
 });
